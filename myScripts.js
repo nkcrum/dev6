d2cb5728ab9b466b6122115a78867c9e47b14d79
nkcrum <48127601+nkcrum@users.noreply.github.com>
2019-10-10 23:17:27 -0500

    Add files via upload

---
+++ myScripts.js
@@ -0,0 +1,9 @@
+<script>
+var person = prompt("Please enter your name", "Harry Potter");
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person + "! How are you today?";
+}
+console.log(person)
+</script>
